6a0fc0a40dff8228b21e350d7297655e3675dc1c
Vitali Naumov <hbvit7@mail.ru>
2015-11-25 13:43:54 +0300

    Add TekEvent constraints

--- grails-app/domain/tekdays/TekEvent.groovy
+++ grails-app/domain/tekdays/TekEvent.groovy
@@ -11,6 +11,13 @@ class TekEvent {
     String description
 
     static constraints = {
+        name()
+        city()
+        description(maxSize:5000)
+        organizer()
+        venue()
+        startDate()
+        endDate()
     }
 
     String toString(){
