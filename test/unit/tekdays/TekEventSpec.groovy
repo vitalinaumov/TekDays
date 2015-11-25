0604fa5fd6c2a7898d693c4832825f405bce25fb
Vitali Naumov <hbvit7@mail.ru>
2015-11-25 12:57:35 +0300

    Add tekEvent domain and test

---
+++ test/unit/tekdays/TekEventSpec.groovy
@@ -0,0 +1,30 @@
+package tekdays
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(TekEvent)
+class TekEventSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test toString"() {
+        when: "a tekEvent has a name and a city"
+            def tekEvent = new TekEvent(name: 'Groovy One',
+                                    city: 'San Francisco, CA',
+                                    organizer: ' John Doe',
+                                    venue: ' Moscone Center',
+                                    startDate: new Date('6/2/2009'),
+                                    endDate: new Date('6/5/2009'),
+                                    description: 'This conference will cover all...')
+        then: "the toString method will combine them."
+        assertEquals 'Groovy One, San Francisco, CA', tekEvent.toString()
+    }
+}
