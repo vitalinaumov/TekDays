010d784fbcb5e80d5dd098c358f214f7d337e669
Vitali Naumov <hbvit7@mail.ru>
2015-11-25 14:55:49 +0300

    Add TekUserDomain

---
+++ grails-app/domain/tekdays/TekUser.groovy
@@ -0,0 +1,20 @@
+package tekdays
+
+class TekUsers {
+
+    String fullName
+    String userName
+    String password
+    String email
+    String website
+    String bio
+
+    static constraints = {
+        fullName()
+        userName()
+        email()
+        website()
+        bio(maxSize:5000)
+    }
+    
+}
